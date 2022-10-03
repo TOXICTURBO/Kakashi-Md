f2e2b15690a93ec3f39f5619d2bda6e3fdc50836
TURBOMODS <94942621+TURBOHYPER@users.noreply.github.com>
2022-10-03 17:55:09 +0530

    Add files via upload

---
+++ plugins/_allfake.js
@@ -0,0 +1,154 @@
+let fs = require('fs')
+let fetch = require('node-fetch')
+let handler = m => m
+handler.all = async function (m) {
+	let pp = 'https://telegra.ph/file/2d06f0936842064f6b3bb.png'
+	try {
+		pp = await this.profilePictureUrl(m.sender, 'image')
+	} catch (e) {
+	} finally {
+		global.axios = require('axios')
+		global.request = require('request')
+		global.users = global.db.data.users[m.sender]
+		global.chats = global.db.data.chats[m.chat]
+        
+		global.ftroli = { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { itemCount: 9999999999999999999999999999999999999999999999999999999, status: 1, surface: 1, message: wm, orderTitle: wm, sellerJid: '0@s.whatsapp.net' } } }
+		
+        global.fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': require('fs').readFileSync('./KakashiMedia/kakashi.jpg'), thumbnail: require('fs').readFileSync('./KakashiMedia/kakashi.jpg'),sendEphemeral: true}}}
+        
+        global.fvn = {
+	 key: { 
+          fromMe: false,
+	      participant: `0@s.whatsapp.net`, ...(m.chat ? 
+	 { remoteJid: "994403219940-1631515755@g.us" } : {}) 
+                },
+	 message: { 
+		"audioMessage": {
+                 "mimetype":"audio/ogg; codecs=opus",
+                 "seconds": "999999999999",
+                 "ptt": "true"
+                        }
+	                  } 
+                     }
+        
+         global.ftextt = {
+	 key: { 
+          fromMe: false,
+	      participant: `0@s.whatsapp.net`, ...(m.chat ? 
+	 { remoteJid: "994403219940-1631515755@g.us" } : {}) 
+                },
+	 message: { 
+		"extendedTextMessage": {
+                 "text":wm,
+                 "title": wm,
+                 'jpegThumbnail': fs.readFileSync('./KakashiMedia/kakashi.jpg')
+                        }
+	                  } 
+                     }
+        
+           global.fliveLoc = {
+	 key:
+	 { fromMe: false,
+	 participant: `0@s.whatsapp.net`, ...(m.chat  ? 
+	 { remoteJid: "status@broadcast" } : {}) },
+	 message: { "liveLocationMessage": { "caption":"by : Turbo And Ajmal","h": `${wm}`, 'jpegThumbnail': fs.readFileSync('./KakashiMedia/kakashi.jpg')}}
+	}
+        
+	       global.fliveLoc2 = {
+	 key:
+	 { fromMe: false,
+	 participant: `0@s.whatsapp.net`, ...(m.chat ? 
+	 { remoteJid: "status@broadcast" } : {}) },
+	 message: { "liveLocationMessage": { "title": "Turbo And Ajmal","h": wm, 'jpegThumbnail': fs.readFileSync('./KakashiMedia/kakashi.jpg')}}
+	}
+        
+            global.ftoko = {
+key: {
+			fromMe: false,
+			participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "2347014889291@s.whatsapp.net" } : {})
+		},
+		message: {
+			"productMessage": {
+				"product": {
+					"productImage":{
+						"mimetype": "image/jpeg",
+						"jpegThumbnail": fs.readFileSync('./KakashiMedia/kakashi.jpg') //Gambarnye
+					},
+					"title": wm, //your name
+					"description": namabot, 
+					"currencyCode": "USD",
+					"priceAmount1000": "20000000",
+					"retailerId": "Ghost",
+					"productImageCount": 1
+				},
+				    "businessOwnerJid": `0@s.whatsapp.net`
+		}
+	}
+}
+        
+              global.fdocs = {
+	key : {
+           participant : '0@s.whatsapp.net'
+                        },
+       message: {
+                    documentMessage: {
+                    title: wm, 
+                    jpegThumbnail: fs.readFileSync('./KakashiMedia/kakashi.jpg')
+                          }
+                        }
+                      }
+        
+             global.fgclink = {
+	"key": {
+		"fromMe": false,
+		"participant": "0@s.whatsapp.net",
+		"remoteJid": "0@s.whatsapp.net"
+	},
+	"message": {
+		"groupInviteMessage": {
+			"groupJid": "994403219940-1631515755@g.us",
+			"inviteCode": "null",
+			"groupName": "𝐓𝚯𝚾𝚰𝐂 ₪ 𝚨𝐋𝚵𝚾𝚨  ⷡ  ⷪ  ⷮⷮ 🪀", 
+            "caption": `╭─${kki}RULES BOT${kka}
+│
+├❒ NO TOXIC
+├❒ NO SPAM
+├❒ DO NOT SEND VIRTEX
+├❒ DO NOT SEND ANYTHING 18+
+├❒ NO PHONE / VC
+├❒ BOT DOES NOT ACCEPT SAVE CONTACTS
+│
+├❏ ANY VIOLATION WILL BE IMMEDIATELY BANNED AND BLOCKED WITHOUT THE LITTLE TOLERANCE
+│
+└─${kki}RULES BOT${kka}`, 
+            'jpegThumbnail': fs.readFileSync('./KakashiMedia/kakashi.jpg')
+		}
+	}
+}
+
+             global.fgif = {
+	 key: { 
+          fromMe: false,
+	      participant: `0@s.whatsapp.net`, ...(m.chat ? 
+	 { remoteJid: "994403219940-1631515755@g.us" } : {}) 
+                },
+	 message: { 
+                 "videoMessage": { 
+                 "title": wm,
+                 "h": `Hmm`,
+                 'seconds': '999999999', 
+                 'gifPlayback': 'true', 
+                 'caption': wm,
+                 'jpegThumbnail': fs.readFileSync('./KakashiMedia/kakashi.jpg')
+                        }
+                       }
+	                  }
+       
+        global.fake = pickRandom(global.fak)
+        
+        global.fak = [ fvn, fgif, ftroli, fgclink, fliveLoc, fliveLoc2, fdocs, ftoko, ftextt, fkontak ]
+		// Add it
+	}
+}
+
+module.exports = handler
---
+++ plugins/_firstchat.js
@@ -0,0 +1,43 @@
+let moment = require('moment-timezone')
+let fetch = require ('node-fetch')
+let handler = m => m
+
+handler.all = async function (m) {
+
+    if (m.chat.endsWith('broadcast')) return
+    if (m.fromMe) return
+    if (m.isGroup) return
+    if (db.data.settings.groupOnly) return
+    let user = global.db.data.users[m.sender]
+    let name = conn.user.name
+    let hao = `*Official Bot By @${'0'.split('@')[0]}* 
+*Powered By @${global.owner[1].split('@')[0]}*`
+    if (new Date - user.pc < 86400000) return //
+        await conn.send3ButtonLoc(m.chat, 'https://i.imgur.com/4rRZ4c7.jpeg', ` ${ucapan()}, @${m.sender.split`@`[0]}
+ ${user.banned ? 'you are banned' : `
+I Am Kakashi, Whatsapp Bot Which You Can Use To Make Stickers, Downloading Something, Playing Games, Etc.
+
+Please Dont Spam Phone Vc Send Virtex.`} 
+ `.trim(), hao, user.banned ? 'Owner' : 'Menu', user.banned ? '!owner' : '.menu', 'User guide', '.tutorbot', 'Verify', '.verify ${conn.getName(m.sender)}.16', m) 
+     user.pc = new Date * 1 
+ } 
+ 
+
+module.exports = handler
+function ucapan() {
+    const time = moment.tz('Asia/Jakarta').format('HH')
+    res = "Good morning"
+    if (time >= 4) {
+        res = "Good morning 🌅"
+    }
+    if (time > 10) {
+        res = "Good afternoon 🏞️"
+    }
+    if (time >= 15) {
+        res = "Good afternoon 🌇"
+    }
+    if (time >= 18) {
+        res = "Good night 🌌"
+    }
+    return res
+}
