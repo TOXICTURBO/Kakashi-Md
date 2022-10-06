319c98da0edc44153651b5c605995f71a3eb25c7
TURBOMODS <94942621+TURBOHYPER@users.noreply.github.com>
2022-10-06 15:05:53 +0530

    Add files via upload

--- plugins/setvideo.js
+++ plugins/setvideo.js
@@ -4,10 +4,6 @@ let mime = m.quoted.mimetype || ''
 let media = await m.quoted.download()
 fs.writeFileSync(`./KakashiMedia/menu.mp4`, media)
 fs.writeFileSync(`./KakashiMedia/kakashi.mp4`, media)
-let setsend = `${global.wait}`
-let out = Buffer.alloc(0)
-}
-await conn.sendFile(m.chat, out, 'out.png', setsend, '', m, false, { thumbnail: out })
 }
 handler.help = ['setvideo','setmenuvideo']
 handler.tags = ['owner']
