0c4d08408083a989770c3a53d674dce5bf4c44b8
TURBOMODS <94942621+TURBOHYPER@users.noreply.github.com>
2022-10-06 15:22:58 +0530

    Add files via upload

--- plugins/setlogo.js
+++ plugins/setlogo.js
@@ -2,12 +2,14 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 if (!m.quoted) throw `reply video with command *${usedPrefix + command}*`
 let mime = m.quoted.mimetype || ''
 let media = await m.quoted.download()
-fs.writeFileSync(`./KakashiMedia/menu.mp4`, media)
-fs.writeFileSync(`./KakashiMedia/Kakashi.mp4`, media)
+fs.writeFileSync(`./KakashiMedia/kakashi.jpg`, media)
+fs.writeFileSync(`./KakashiMedia/logo.jpg`, media)
+fs.writeFileSync(`./src/logo.jpg`, media)
+fs.writeFileSync(`./thumbnail.jpg`, media)
 m.reply(`*Done ✔️*`, ftoko)
 }
-handler.help = ['setvideo','setmenuvideo']
+handler.help = ['setlogo','setthumb']
 handler.tags = ['owner']
-handler.command = /^(setvideo|setmenuvideo)$/i
+handler.command = /^(setlogo|setthumb)$/i
 
 module.exports = handler
