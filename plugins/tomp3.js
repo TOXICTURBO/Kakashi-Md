fe4941f2cafda5564c33645cc82e13a69b1ef0cf
TURBOMODS <94942621+TURBOHYPER@users.noreply.github.com>
2022-10-10 02:22:26 +0530

    Add files via upload

--- plugins/tomp3.js
+++ plugins/tomp3.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
     if (!media) throw 'Media can\'t be downloaded'
     let audio = await toAudio(media, 'mp4')
     if (!audio.data) throw 'Failed to perform conversion.'
-    await conn.sendFile(m.chat, audio.data, 'file.mp3', '', m, 0, { mimetype: 'audio/mpeg', asDocument: false })
+    await conn.sendFile(m.chat, audio.data, 'file.mp3', '', m, 0, { mimetype: 'audio/mp4', asDocument: false })
   }
   if (/vn|ptt$/i.test(command)) {
     if (!/video|audio/.test(mime)) throw `Reply video/audio with command *${usedPrefix + command}*`
