ce7943fa2d991aa9372186d257c22d41245a6c08
TURBOMODS <94942621+TURBOHYPER@users.noreply.github.com>
2022-10-14 19:20:01 +0530

    Add files via upload

--- plugins/yta.js
+++ plugins/yta.js
@@ -5,16 +5,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   let chat = global.db.data.chats[m.chat]
   let server = (args[1] || servers[0]).toLowerCase()
   let { dl_link, thumb, title, filesize, filesizeF } = await yta(args[0], servers.includes(server) ? server : servers[0])
-  conn.sendFile(m.chat, dl_link, title + '.mp3', `
-â”â”‰â”â”â”â”â”â”â”â”â”â”â”â
-â”† *YOUTUBE MP3*
-â”œâ”ˆâ”ˆâ”ˆâ”ˆâ”ˆâ”ˆâ”ˆâ”ˆâ”ˆâ”ˆâ”ˆ
-â”†â€¢ *Title:* ${title}
-â”‚â€¢ *Type:* MP3
-â”†â€¢ *ðŸ“¥ File Size:* ${filesizeF}
-â””â
-`.trim(), m, null, {
-    asDocument: chat.useDocument, mimetype: 'audio/mp4', ptt: false, contextInfo: {
+  conn.sendMessage(m.chat, { document: { url: dl_link }, mimetype: 'audio/mp4', fileName: title + `.mp3`, contextInfo: {
         externalAdReply: {
             title: `${title}`, 
             body: 'Now Playing...',
@@ -29,15 +20,5 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 handler.help = ['mp3', 'a'].map(v => 'yt' + v + ` <url>`)
 handler.tags = ['downloader']
 handler.command = /^yt(a|mp3)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
 
 module.exports = handler
