52ef4709d42db010ec4c7653cedcecafc89e2fe1
TURBOMODS <94942621+TURBOHYPER@users.noreply.github.com>
2022-10-14 19:00:22 +0530

    Add files via upload

--- plugins/yta.js
+++ plugins/yta.js
@@ -1,14 +1,11 @@
-let axios = require('axios')
-const fetch = require('node-fetch')
-let limit = 1024354
+let fetch = require('node-fetch')
 const { servers, yta } = require('../lib/y2mate')
-let handler = async (m, { conn, args, isPrems, isOwner }) => {
-  if (!args || !args[0]) throw 'Uhm... where is the url?'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  if (!args || !args[0]) throw `Exmple:\n${usedPrefix + command} url`
   let chat = global.db.data.chats[m.chat]
   let server = (args[1] || servers[0]).toLowerCase()
-  let { dl_link, thumb, title, filesize, filesizeF} = await yta(args[0], servers.includes(server) ? server : servers[0])
-  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize 
-  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp3', `
+  let { dl_link, thumb, title, filesize, filesizeF } = await yta(args[0], servers.includes(server) ? server : servers[0])
+  conn.sendFile(m.chat, dl_link, title + '.mp3', `
 â”â”‰â”â”â”â”â”â”â”â”â”â”â”â
 â”† *YOUTUBE MP3*
 â”œâ”ˆâ”ˆâ”ˆâ”ˆâ”ˆâ”ˆâ”ˆâ”ˆâ”ˆâ”ˆâ”ˆ
@@ -17,16 +14,30 @@ let handler = async (m, { conn, args, isPrems, isOwner }) => {
 â”†â€¢ *ðŸ“¥ File Size:* ${filesizeF}
 â””â
 `.trim(), m, null, {
-  asDocument: chat.useDocument
-})
+    asDocument: chat.useDocument, mimetype: 'audio/mp4', ptt: false, contextInfo: {
+        externalAdReply: {
+            title: `${title}`, 
+            body: 'Now Playing...',
+            description: 'Now Playing...',
+            mediaType: 2,
+          thumbnail: await (await fetch(thumb)).buffer(),
+         mediaUrl: `https://instagram/toxic_turbo777`
+        }
+     }
+  })
 }
-handler.help = ['mp3','a'].map(v => 'yt' + v + ` <url>`)
+handler.help = ['mp3', 'a'].map(v => 'yt' + v + ` <url>`)
 handler.tags = ['downloader']
 handler.command = /^yt(a|mp3)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
 
 handler.fail = null
-handler.exp = 0
-handler.limit = true
 
 module.exports = handler
-
--- plugins/ytv.js
+++ plugins/ytv.js
@@ -1,45 +1,21 @@
-let limit = 1024
 let fetch = require('node-fetch')
 const { servers, ytv } = require('../lib/y2mate')
-let handler = async (m, { conn, args, isPrems, isOwner }) => {
-  if (!args || !args[0]) throw 'Uhm... where is the url?'
-  let chat = global.db.data.chats[m.chat]
-  let server = (args[1] || servers[0]).toLowerCase()
-  let { dl_link, thumb, title, filesize, filesizeF} = await ytv(args[0], servers.includes(server) ? server : servers[0])
-  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-  let name = await conn.getName(m.sender)
-let wm = global.wm
-  conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
+let handler = async(m, { conn, args, isPrems, isOwner }) => {
+    if (!args || !args[0]) return conn.reply(m.chat, 'Uhm... the url?', m)
+    let chat = global.db.data.chats[m.chat]
+    let server = (args[1] || servers[0]).toLowerCase()
+    let { dl_link, thumb, title, filesize, filesizeF } = await ytv(args[0], servers.includes(server) ? server : servers[0])
+    let _thumb = {}
+    try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } } catch (e) {}
+    m.reply(wait)
+	await conn.sendMessage(m.chat, { document: { url: dl_link }, mimetype: 'video/mp4', fileName: title + `.mp4`}, {quoted: m})
+   conn.sendFile(m.chat, dl_link, title + '.mp4', `
 *Title:* ${title}
 *Filesize:* ${filesizeF}
-*${isLimit ? 'Use ': ''}Link:* ${dl_link}
-`.trim(), m)
-  let _thumb = {}
-  try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
-  catch (e) { }
-  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp4', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-`.trim(), m, false, {
-  ..._thumb,
-  asDocument: chat.useDocument
-})
+`.trim(), m, false, { thumbnail: Buffer.alloc(0), mimetype: 'video/mp4' })
 }
-handler.help = ['mp4','v',''].map(v => 'yt' + v + ` <url>`)
+handler.help = ['ytmp4 <query>']
 handler.tags = ['downloader']
-handler.command = /^yt(v|mp4)?$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.exp = 0
-handler.limit = true
+handler.command = /^yt(v(idi?e?o)?|mp4)?$/i
 
 module.exports = handler
-
