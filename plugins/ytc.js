3ddcda9b8293f8fef2687507ce3c46154537d48e
TURBOMODS <94942621+TURBOHYPER@users.noreply.github.com>
2022-11-08 16:01:36 +0530

    Add files via upload

--- plugins/ytc.js
+++ plugins/ytc.js
@@ -1,13 +1,12 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, usedPrefix, command }) => {
 let res = await fetch('https://api.lolhuman.xyz/api/ytcomment?apikey=5e0b5cf41125348368c4f017&username=name&comment=teks&img=pp')
-let json = await res.json()
 let media = await m.quoted.download()
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let { registered } = global.db.data.users[m.sender]
 let name = registered ? global.db.data.users[m.sender].name : conn.getName(m.sender) 
 let pp = await conn.profilePictureUrl(who, 'image').catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
-conn.reply(m.chat, json.url, m)
+conn.reply(m.chat, res, 'res.png', '', m, false, { thumbnail: res }))
 }
 handler.help = ['ytc <teks>']
 handler.tags = ['sticker']
