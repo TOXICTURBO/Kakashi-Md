9d7ff241935c5d206d3fd9279ae2cb00534fabc8
Toxic-Turbo <97358745+TOXICTURBO@users.noreply.github.com>
2023-02-19 21:33:14 +0530

    Update textpro-glitch.js

--- plugins/textpro-glitch.js
+++ plugins/textpro-glitch.js
@@ -5,9 +5,9 @@ let split = '|'
 let [text, ...text2] = txt.replace(dude, '').trimStart().split(split)
   text2 = text2.join(split)
 if (!text) throw `Enter Text
-Example : ${_p}${command} Turbo|Ajmal`
+Example : ${_p}${command} Turbo`
 if (!text2) throw `Enter Text
-Example : ${_p}${command} Turbo|Ajmal`
+Example : ${_p}${command} Turbo`
    m.reply('_Process..._') 
    let res = `https://api.xteam.xyz/textpro/glitch?text=${text}&text2=${text2}&APIKEY=bf8ff984af1506b7` 
    conn.sendFile(m.chat, res, `${command}`, wm3, m, false) 
